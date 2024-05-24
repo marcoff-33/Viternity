32f01fafdb249ab1212dea59398790a63d0bada7
marcoff <marcoff033@gmail.com>
2024-05-24 13:13:48 +0200

    feat: implemented text editor & typography

--- src/app/[vault]/page.tsx
+++ src/app/[vault]/page.tsx
@@ -1,12 +1,16 @@
 import { ImagesCarousel } from "@/components/ImagesCarousel";
+import Tiptap from "@/components/TipTap";
 
 import React from "react";
 
 export default async function page() {
   return (
-    <div className="bg-red-50 flex justify-center h-screen">
+    <div className="bg-red-50 flex justify-center h-screen flex-col container">
       <div className="items-center max-h-fit flex justify-center">
         <ImagesCarousel />
+      </div>
+      <div className="border border-red-500 ">
+        <Tiptap />
       </div>
     </div>
   );
