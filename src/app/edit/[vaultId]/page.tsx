ce029250288b6aeb97957ac90df3f4fe1fb7b8b6
marcoff <marcoff033@gmail.com>
2024-05-30 11:41:56 +0200

    feat: implemented qr code generator in edit mode

--- src/app/edit/[vaultId]/page.tsx
+++ src/app/edit/[vaultId]/page.tsx
@@ -1,15 +1,19 @@
+import QrCodeModal from "@/components/QrCodeModal";
 import VaultTemplate from "@/components/VaultTemplate";
-import VaultEditor from "@/components/vaultEditor";
 import { auth } from "@clerk/nextjs/server";
-import { usePathname } from "next/navigation";
 import React from "react";
 
 export default function page() {
   const { userId } = auth();
   return (
-    <div className="bg-red-50 flex justify-center min-h-screen flex-col container">
+    <div className="bg-red-50 flex justify-center min-h-screen flex-col container relative">
       {userId ? (
-        <VaultTemplate isEditable={true} userId={userId} />
+        <>
+          <VaultTemplate isEditable={true} userId={userId} />
+          <div className="sticky bottom-20 right-[50%]">
+            <QrCodeModal />
+          </div>
+        </>
       ) : (
         <div className="">You need to be logged in to edit this vault.</div>
       )}
---
+++ src/components/QrCodeModal.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import React from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Button } from "./ui/button";
+
+export default function QrCodeModal() {
+  const pathName = usePathname();
+  const vaultId = pathName.split("/")[pathName.split("/").length - 1];
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant={"secondary"}>QR Code</Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <img
+            src={`https://api.qrserver.com/v1/create-qr-code/?size=350x350&data=https://localhost:3000/${vaultId}`}
+            className="p-2 border-b"
+          />
+          <DialogDescription className="p-2">
+            To save your QR Code locally, Right Click on the image then select
+            "Save as...".
+          </DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
+}
