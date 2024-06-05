ac3bb0ae77fa6142aa46f8de0f5985f0352d4636
marcoff <marcoff033@gmail.com>
2024-06-05 03:12:11 +0200

    fix: minor text color fix

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Navbar() {
   return (
     <div className="container flex justify-between items-center absolute z-50">
-      <div className="flex gap-5">
+      <div className="text-white flex gap-5">
         <a href="/">Home</a>
         <a href="/">Dashboard</a>
       </div>
