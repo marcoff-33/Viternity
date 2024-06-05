9b5f8cb3248566b1b0a39907a219a8a960012b49
marcoff <marcoff033@gmail.com>
2024-06-05 04:17:02 +0200

    fix: devlog page color fix

--- src/app/devlog/page.tsx
+++ src/app/devlog/page.tsx
@@ -2,11 +2,11 @@ import React from "react";
 
 export default function page() {
   return (
-    <div className="flex justify-start min-w-full min-h-screen flex-col container">
-      <div className="text-4xl font-bold text-black self-start min-w-full text-center">
+    <div className="flex justify-start min-w-full min-h-screen flex-col container bg-accent-foreground">
+      <div className="text-4xl font-bold text-accent self-start min-w-full text-center">
         Changelong
       </div>
-      <div className="text-black flex flex-row gap-10 justify-center">
+      <div className="text-accent flex flex-row gap-10 justify-center">
         <h2>0.0.1:</h2>
         <p>
           Aggiornata Pagina Principale, Aggiunta animazione quando si crea un
