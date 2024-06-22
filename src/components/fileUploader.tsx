af9e4be425320e6f90a5cade1842817e336981fb
marcoff <marcoff033@gmail.com>
2024-06-22 05:51:15 +0200

    fix: fixed broken upload photo button on mobile

--- src/components/fileUploader.tsx
+++ src/components/fileUploader.tsx
@@ -63,7 +63,7 @@ export default function FileUploader({
     <div className="self-center flex justify-center text-center items-center font-semibold sm:text-lg relative bg-accent-foreground text-primary">
       <input
         type="file"
-        className="cursor-pointer text-transparent file:hidden absolute flex justify-center items-center text-center max-w-full"
+        className="z-50 cursor-pointer text-transparent file:hidden absolute flex justify-center items-center text-center max-w-full"
         onChange={(event) => {
           handleFileChange(event);
         }}
